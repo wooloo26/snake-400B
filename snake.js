f21bb68ce615c6a1dd88715d14c6a7b0054788de
Wooloo <ayan0312000@gmail.com>
2025-09-06 11:35:58 +0800

    simple minify

--- snake.js
+++ snake.js
@@ -1,27 +1,31 @@
-(() => {
-    var doc = document, rand = Math.random, n = 20, s = 4, snake = [{ x: 10, y: 10 }], dir = { x: 1, y: 0 }, food, ctx = doc.getElementById('g').getContext('2d'), respawn = () => food = { x: rand() * n | 0, y: rand() * n | 0 };
-    respawn();
-
-
-    setInterval(() => {
-        let head = { x: snake[0].x + dir.x, y: snake[0].y + dir.y };
-        if ((head.x | head.y) < 0 || head.x >= n || head.y >= n || snake.some(t => t.x == head.x && t.y == head.y))
-            throw 1;
-
-        snake.unshift(head);
-        head.x == food.x && head.y == food.y ? respawn() : snake.pop();
-
-        ctx.clearRect(0, 0, 80, 80);
-        ctx.fillStyle = 'red';
-        ctx.fillRect(food.x * s, food.y * s, s, s);
-        ctx.fillStyle = 'green';
-        snake.forEach((seg, i) => (ctx.globalAlpha = i ? .7 : 1, ctx.fillRect(seg.x * s, seg.y * s, s, s)));
-        ctx.globalAlpha = 1;
-    }, 120);
-
-    doc.onkeydown = e => {
-        let k = { ArrowUp: [0, -1], ArrowDown: [0, 1], ArrowLeft: [-1, 0], ArrowRight: [1, 0] }[e.key];
-        if (k && (snake.length < 2 || snake[0].x + k[0] != snake[1].x || snake[0].y + k[1] != snake[1].y))
-            dir = { x: k[0], y: k[1] };
-    };
-})()
+d = document
+g = 9
+r = () => Math.random() * g | 0
+p = (points) => {
+    do z = { x: r(), y: r() }
+    while (points.some(t => t.x == z.x && t.y == z.y))
+    return z
+}
+w = 1
+s = [{ x: 1, y: 1 }]
+m = { x: 1, y: 0 }
+c = d.getElementById('g').getContext('2d')
+n = () => f = p(s)
+f = p(s)
+setInterval(() => {
+    h = { x: s[0].x + m.x, y: s[0].y + m.y }
+    if ((h.x | h.y) < 0 || h.x >= g || h.y >= g || s.some(t => t.x == h.x && t.y == h.y))
+        _
+    s.unshift(h)
+    h.x == f.x && h.y == f.y ? n() : s.pop()
+    c.clearRect(0, 0, 9, 9)
+    c.fillStyle = 'red'
+    c.fillRect(f.x * w, f.y * w, w, w)
+    c.fillStyle = 'green'
+    s.forEach((seg) => c.fillRect(seg.x * w, seg.y * w, w, w))
+}, 99)
+d.onkeydown = e => {
+    k = { ArrowUp: [0, -1], ArrowDown: [0, 1], ArrowLeft: [-1, 0], ArrowRight: [1, 0] }[e.key]
+    if (k && (s.length < 2 || s[0].x + k[0] != s[1].x || s[0].y + k[1] != s[1].y))
+        m = { x: k[0], y: k[1] }
+}
