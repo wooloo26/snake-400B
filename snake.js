6f2785f494499f935f5b715be57b412fed6bbdce
Wooloo <ayan0312000@gmail.com>
2025-09-06 11:44:21 +0800

    reduce fillRect calls

--- snake.js
+++ snake.js
@@ -12,17 +12,18 @@ m = { x: 1, y: 0 }
 c = d.getElementById('g').getContext('2d')
 n = () => f = p(s)
 f = p(s)
+e = (point) => c.fillRect(point.x * w, point.y * w, w, w)
 setInterval(() => {
     h = { x: s[0].x + m.x, y: s[0].y + m.y }
     if ((h.x | h.y) < 0 || h.x >= g || h.y >= g || s.some(t => t.x == h.x && t.y == h.y))
         _
-    s.unshift(h)
+    s = [h, ...s]
     h.x == f.x && h.y == f.y ? n() : s.pop()
     c.clearRect(0, 0, 9, 9)
     c.fillStyle = 'red'
-    c.fillRect(f.x * w, f.y * w, w, w)
-    c.fillStyle = 'green'
-    s.forEach((seg) => c.fillRect(seg.x * w, seg.y * w, w, w))
+    e(f)
+    c.fillStyle = 'blue'
+    s.forEach(e)
 }, 99)
 d.onkeydown = e => {
     k = { ArrowUp: [0, -1], ArrowDown: [0, 1], ArrowLeft: [-1, 0], ArrowRight: [1, 0] }[e.key]
